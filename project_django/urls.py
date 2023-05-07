46757a5889755a9265d9da806d3a7c2704ad1de6
aliccciao <alicia.utomo@gmail.com>
2023-05-07 10:56:15 +0700

    authentication & navbar DONE

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
+    path('', include('kira.urls')),
     path('atlet/', include('atlet.urls')),
 ]
