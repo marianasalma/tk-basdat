b743b2363f165ddadf00494c7b64655e477cf40a
Mariana Salma <marianasalma11@gmail.com>
2023-05-08 08:12:54 +0700

    finish umpire

--- umpire/urls.py
+++ umpire/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from umpire.views import show_semi
+from umpire.views import show_final
+from umpire.views import show_hasil
 from umpire.views import show_perempat
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('perempat', show_perempat, name='show_perempat'),
+    path('semi', show_semi, name='show_semi'),
+    path('final', show_final, name='show_final'),
+    path('hasil', show_hasil, name='show_hasil'),
+
+
 ]
--- umpire/views.py
+++ umpire/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def show_perempat(request):
     return render(request, "perempat.html")
+
+
+def show_semi(request):
+    return render(request, "semi.html")
+
+
+def show_final(request):
+    return render(request, "final.html")
+
+
+def show_hasil(request):
+    return render(request, "hasil.html")
